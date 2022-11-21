4f6e7ab6c6d1674cd9153e672f9d5b91a250aaac
Дамир <damir.obuhov@mail.ru>
2022-11-22 00:07:03 +0400

    Задание 1  Классы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+        Author author1 = new Author("Dan Brown", "Brown");
+        Book book1 = new Book("Later", author1, 2003);
+
+        System.out.println("book1.bookName = " + book1.getBookName() + "\n" + "author1.authorFirstName = " +
+                author1.getAuthorFirstName() + "\n" + "author1.authorLastName = " + author1.getAuthorLastName() + "\n" +
+                "book1.publishingYear = " + book1.getPublishingYear() + "\n");
+
+        book1.setPublishingYear(2003);
+        System.out.println("book1.getPublishingYear() = " + book1.getPublishingYear());
+
+        System.out.println();
+
+        Author author2 = new Author("Steven", "King");
+        Book book2 = new Book("Harry Potter and the Chamber of Secrets", author2, 1996);
+
+        System.out.println("book2.bookName = " + book2.getBookName() + "\n" + "author2.authorFirstName = " +
+                author2.getAuthorFirstName() + "\n" + "author2.authorLastName = " + author2.getAuthorLastName() + "\n" +
+                "book2.publishingYear = " + book2.getPublishingYear() + "\n");
+
+        book2.setPublishingYear(1996);
+        System.out.println("book2.getPublishingYear() = " + book2.getPublishingYear());
     }
 }
